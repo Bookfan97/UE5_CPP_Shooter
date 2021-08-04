a27fcb89f8c444fb1d3c39dc05823e1a243d06a4
Noah Day <sheltielover432@gmail.com>
2021-08-04 19:34:46 -0400

    Shooting Animation

--- Shooter/Source/Shooter/ShooterCharacter.h
+++ Shooter/Source/Shooter/ShooterCharacter.h
@@ -72,6 +72,10 @@ private:
 	/*Flash spawned at BarrelSocket*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
 	class UParticleSystem* MuzzleFlash;
+
+	/*Montage for firing weapon*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class UAnimMontage* HipFireMontage;
 public:
 	/*Returns camera boom subobject*/
 	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
